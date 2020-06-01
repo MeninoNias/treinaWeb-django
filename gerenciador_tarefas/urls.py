05fada9606cd4c021b702b6190dcc9b36ce2c516
Ananias <ananias.nobrega@gmail.com>
2020-06-01 03:24:08 -0300

    deploy on heroku

--- gerenciador_tarefas/urls.py
+++ gerenciador_tarefas/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls', namespace='app')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + staticfiles_urlpatterns()
